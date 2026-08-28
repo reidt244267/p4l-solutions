74de038c1b2441c01f490d272de84d0f2515a2fd
reidt244267 <reidt@andrew.cmu.edu>
2026-08-28 12:24:41 -0400

    Complete Factorial in python

---
+++ chapter_0/factorial/factorial.py
@@ -0,0 +1,14 @@
+# Insert your factorial() function here.
+def factorial(n: int) -> int:
+    """
+    Compute n! (factorial) using a while loop.
+    Args:
+        n: A non-negative integer.
+    Returns:
+        The factorial of n.
+    """
+
+    fact=1
+    for i in range(n):
+        fact=fact*(i+1)
+    return fact
