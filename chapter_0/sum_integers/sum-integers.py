20e58a41a80a69320ace50a87e517e8df312d1ac
reidt244267 <reidt@andrew.cmu.edu>
2026-09-01 10:45:16 -0400

    Complete SumIntegers in python

---
+++ chapter_0/sum_integers/sum-integers.py
@@ -0,0 +1,13 @@
+# Insert your sum_integers() function here, along with any subroutines that you need.
+def sum_integers(*numbers: int) -> int:
+    """
+    Return the sum of a variable number of integers.
+    Args:
+        numbers: Zero or more integers.
+    Returns:
+        The sum of all provided integers. Returns 0 if no arguments are given.
+    """
+    sum=0
+    for val in numbers:
+        sum+=val
+    return sum
