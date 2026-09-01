6c4f484cb5954aee29cd67e310f975ec7dd1a5a5
reidt244267 <reidt@andrew.cmu.edu>
2026-09-01 19:25:09 -0400

    Complete SumProperDivisors in python

---
+++ chapter_0/sum_proper_divisors/sum-proper-divisors.py
@@ -0,0 +1,17 @@
+# Insert your sum_proper_divisors() function here, along with any subroutines that you need.
+def sum_proper_divisors(n: int) -> int:
+    """
+    Return the sum of all proper (positive) divisors of n, i.e., divisors strictly less than n.
+    Args:
+        n: Integer input.
+    Returns:
+        The sum of all positive divisors of n that are < n.
+        Returns 0 for n <= 1.
+    """
+
+    d=0
+    for i in range (1,n):
+        if (n%(i)==0):
+            d=d+i
+
+    return d
