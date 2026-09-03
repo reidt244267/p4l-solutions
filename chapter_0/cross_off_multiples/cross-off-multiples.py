a62a772f4dd9c5d42bcb5c40d52ef85d7a05bb6d
reidt244267 <reidt@andrew.cmu.edu>
2026-09-03 16:54:31 -0400

    Complete CrossOffMultiples in python

---
+++ chapter_0/cross_off_multiples/cross-off-multiples.py
@@ -0,0 +1,16 @@
+# Insert your cross_off_multiples() function here, along with any subroutines that you need.
+def cross_off_multiples(prime_booleans: list[bool], p:int) -> list[bool]:
+    """
+    Takes as input a list of booleans representing the primality of all integers
+    up to some point.
+
+    Also takes as input an integer p
+
+    Updates the list by setting to False all multiples of p (starting at 2*p)
+    """
+    #range over multiples
+    for i in range(p+p, len(prime_booleans),p):
+        #cross off the current number cuz we know i is a multiple of p
+        prime_booleans[i]=False
+
+    return prime_booleans
