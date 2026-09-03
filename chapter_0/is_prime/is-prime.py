ccad4140d628c05df8c1360f9bad7441c83f12c5
reidt244267 <reidt@andrew.cmu.edu>
2026-09-03 11:03:27 -0400

    Complete IsPrime in python

---
+++ chapter_0/is_prime/is-prime.py
@@ -0,0 +1,25 @@
+import math
+# Insert your is_prime() function here, along with any subroutines that you need.
+def is_prime(k: int) -> bool:
+    """
+    Determine whether an integer is prime.
+    Args:
+        p: Integer to test (may be negative or zero).
+    Returns:
+        True if p is prime, False otherwise.
+    """
+
+    if k<0:
+        raise ValueError("Error: k must be nonnegative")
+
+    if k<2:
+        return False
+
+    #try every number between 2 and k-1
+    #if it's a disvior, return False
+    #can stop range at sqrt(k)
+    for i in range (2,math.isqrt(k)+1):
+        if k%i==0:
+            return False
+
+    return True
