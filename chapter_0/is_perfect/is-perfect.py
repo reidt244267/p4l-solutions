82d64f88378a858ac8319796b47ca3a727e62521
reidt244267 <reidt@andrew.cmu.edu>
2026-09-04 12:40:47 -0400

    Complete IsPerfect in python

---
+++ chapter_0/is_perfect/is-perfect.py
@@ -0,0 +1,31 @@
+# Provided for you (from an earlier exercise):
+def sum_proper_divisors(n: int) -> int:
+    """
+    Return the sum of all proper (positive) divisors of n, i.e., divisors strictly less than n.
+    Args:
+        n: Integer input.
+    Returns:
+        The sum of all positive divisors of n that are < n.
+        Returns 0 for n <= 1.
+    """
+    # sum all divisors of n
+    divisors_sum = 0
+    for i in range(1, n):
+        if n % i == 0:
+            divisors_sum += i
+    return divisors_sum
+
+
+# Insert your is_perfect() function here, along with any subroutines that you need.
+def is_perfect(n: int) -> bool:
+    """
+    Determine whether an integer n is a perfect number.
+    Args:
+        n: Integer to test.
+    Returns:
+        True if n is perfect, False otherwise.
+    """
+    if n==sum_proper_divisors(n):
+        return True
+    else:
+       return False
