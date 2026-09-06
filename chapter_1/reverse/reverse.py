05fdcbd1b2723c883f0e5b1e43f9448520fb107f
reidt244267 <reidt@andrew.cmu.edu>
2026-09-06 08:43:21 -0400

    Complete Reverse in python

---
+++ chapter_1/reverse/reverse.py
@@ -0,0 +1,17 @@
+# Write your reverse() function here.
+def reverse(s: str) -> str:
+    """
+    reverse returns the given string backwards.
+
+    Parameters:
+    - s (str): The given string to reverse.
+
+    Returns:
+    - str: The reverse of s.
+    """
+    lst=''
+    for char in s:
+        lst=char+lst
+
+
+    return lst
