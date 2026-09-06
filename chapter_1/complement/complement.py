49342bfc79da3e9460cd845712aaa609d7320fd7
reidt244267 <reidt@andrew.cmu.edu>
2026-09-06 08:42:24 -0400

    Complete Complement in python

---
+++ chapter_1/complement/complement.py
@@ -0,0 +1,26 @@
+# Insert your complement() function here.
+def complement(dna: str) -> str:
+    """
+    Finds the complementary strand of the given string.
+
+    Parameters:
+    - dna (str): A dna string.
+
+    Returns:
+    - str: the string whose i-th symbol is the complementary 
+    nucleotide of the i-th symbol of the input string. (A-T, C-G, T-A, G-C).
+    """
+    lst=''
+    for char in dna:
+        if char=='A':
+            lst=lst+'T'
+        elif char=='T':
+            lst=lst+'A'
+        elif char=='G':
+            lst=lst+'C'
+        elif char=='C':
+            lst=lst+'G'
+
+
+
+    return lst
