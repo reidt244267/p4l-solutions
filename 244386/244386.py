001330232028b20784e82c93af3a98fc2b8fce88
reidt244267 <reidt@andrew.cmu.edu>
2026-09-08 08:24:09 -0400

    Complete 244386 in python

---
+++ 244386/244386.py
@@ -0,0 +1,16 @@
+import math
+# Insert your bacteria_hours() function here.
+def bacteria_hours(n: int) -> int:
+    """
+    Calculate the number of hours required for a bacteria population 
+    (starting at size 1 and doubling every hour) to reach at least size n.
+
+    Parameters: n (int) - The target population size (must be >= 1).
+
+
+
+    Returns: time (int) - The number of hours until the population is at least size n.
+    """
+    
+
+    return math.ceil(math.log(n,2))
