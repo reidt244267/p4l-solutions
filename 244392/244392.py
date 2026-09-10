d23449efb51fddb5a887d8e10425b6d327689d8e
reidt244267 <reidt@andrew.cmu.edu>
2026-09-10 08:13:51 -0400

    Complete 244392 in python

---
+++ 244392/244392.py
@@ -0,0 +1,17 @@
+# Insert your prefixes() function here.
+def prefixes(s: str) -> list[str]:
+    """
+    Return a list of all prefixes of the string s.
+
+    Parameters:
+        s (str) - The input string.
+
+    Returns:
+        list[str] - A list of prefixes of s, starting with the empty string ""
+                    and ending with s itself.
+    """
+    lst=[]
+    for i in range(0,len(s)+1):
+        lst.append(s[0:i])
+
+    return lst
