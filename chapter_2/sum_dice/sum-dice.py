4010d0793fd3e70f2da32256c5f6c712a0fd9816
reidt244267 <reidt@andrew.cmu.edu>
2026-09-10 10:13:14 -0400

    Complete SumDice in python

---
+++ chapter_2/sum_dice/sum-dice.py
@@ -0,0 +1,12 @@
+import math
+import random # this should be helpful!
+
+# Write your sum_dice() function here along with any subroutines that you need.
+def sum_dice(num_dice : int) -> int:
+    """
+    Simulate the roll of num_dice dice.
+
+    Returns:
+    int: The sum of num_dice pseudorandom integers between 1 and 6 inclusively.
+    """
+    return random.randint(1,6)+random.randint(1,6)
