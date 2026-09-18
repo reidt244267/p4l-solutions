8a240a81e44a72a5fd44941c839272c8297a785f
reidt244267 <reidt@andrew.cmu.edu>
2026-09-18 12:27:39 -0400

    Complete HasRepeat in python

---
+++ chapter_2/has_repeat/has-repeat.py
@@ -0,0 +1,17 @@
+# Write your has_repeat() function here along with any subroutines that you need
+def has_repeat(a: list[int]) -> bool:
+    """
+    Check if a list has repeat elements.
+
+    Parameters:
+    - a: a list of integers
+
+    Returns:
+    bool: True if a has repeat elements, False otherwise
+    """
+    for key,value1 in enumerate(a):
+        for i in range(key+1,len(a)):
+            if value1==a[i]:
+                return True
+
+    return False
